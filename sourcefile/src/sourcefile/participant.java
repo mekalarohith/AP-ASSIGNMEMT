a34a345114245efeb376e8c7db8ed22b3db3efec
sairao <sairao@192.168.1.4>
2017-09-06 02:01:19 +1000

    added participant

---
+++ sourcefile/src/sourcefile/participant.java
@@ -0,0 +1,46 @@
+package sourcefile;
+
+public class participant {
+		private String ID;
+		private String name;
+		private int age;
+		private String state;
+
+		public participant(String ID, String name, int age, String state){
+			this.ID = ID;
+			this.setName(name);
+			this.setAge(age);
+			this.setState(state);
+		}
+		
+		
+		public String getID() {
+			return ID;
+		}
+		public void setID(String ID) {
+			this.ID = ID;
+		}
+				public String getName() {
+			return name;
+		}
+		
+		public void setName(String name) {
+			this.name = name;
+		}
+		
+		public int getAge() {
+			return age;
+		}
+		
+		public void setAge(int age) {
+			this.age = age;
+		}
+		
+		public String getState() {
+			return state;
+		}
+		
+		public void setState(String state) {
+			this.state = state;
+		}	
+	}
