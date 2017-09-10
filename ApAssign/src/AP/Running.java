21d7aabae9b773b77b919cbb2e3c54bb14bb4b13
lenovo <lenovo@192.168.1.14>
2017-09-11 05:00:56 +1000

    added running class

---
+++ ApAssign/src/AP/Running.java
@@ -0,0 +1,27 @@
+package AP;
+
+//Author is Rohith
+//this class for running with override method from superclass Sports.class
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Running extends Sports {
+
+
+	
+public Running(String sportsID, Referee referee, ArrayList<CompeteResult> competeResults) {
+		super(sportsID, referee, competeResults);
+		// TODO Auto-generated constructor stub
+	}
+
+	/*
+* generate a random time for Running from 10 to 20sec.
+*/
+	@Override
+	public int computeTime() {
+		
+		Random r = new Random();
+		
+		return r.nextInt(11)+10;
+	}
+}
